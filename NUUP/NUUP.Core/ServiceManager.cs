8739d9f3b678f977e773181b7713b0becb59efed
Juan Carlos Fontecha <jcfontecha@gmail.com>
2017-03-06 17:13:09 -0600

    Initial commit

---
+++ NUUP/NUUP.Core/ServiceManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NUUP.Core
+{
+    public class ServiceManager
+    {
+        private HttpClient Client { get; set; }
+        public string BaseURL { get; set; }
+
+        public ServiceManager()
+        {
+            Client = new HttpClient();
+            BaseURL = "http://ec2-54-197-40-152.compute-1.amazonaws.com/api/v2";
+
+            Client.BaseAddress = new Uri("http://ec2-54-197-40-152.compute-1.amazonaws.com/api/v2/");
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
---
+++ NUUP/Nuup.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Nuup.Core.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Nuup.Core.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("41d2e650-07de-46c8-b40b-3eb48adf1cd2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
